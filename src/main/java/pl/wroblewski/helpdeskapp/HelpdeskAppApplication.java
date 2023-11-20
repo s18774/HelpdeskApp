2fda5fd28b6cf6bd42e4d25c82ab9122c6b7df99
s18774 <s18774@pjwstk.edu.pl>
2023-11-20 20:43:19 +0100

    Initial commit

---
+++ src/main/java/pl/wroblewski/helpdeskapp/HelpdeskAppApplication.java
@@ -0,0 +1,13 @@
+package pl.wroblewski.helpdeskapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskAppApplication.class, args);
+	}
+
+}
