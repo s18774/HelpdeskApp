337055516ae0b2865c4c308ef4a2b5dd11eeab3e
s18774 <s18774@pjwstk.edu.pl>
2023-12-04 18:14:43 +0100

    logsRepository added, still empty, WIP

---
+++ src/main/java/pl/wroblewski/helpdeskapp/repositories/LogsRepository.java
@@ -0,0 +1,7 @@
+package pl.wroblewski.helpdeskapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.wroblewski.helpdeskapp.models.Logs;
+
+public interface LogsRepository extends CrudRepository<Logs,Integer> {
+}
