a1197b047678a421c6c1979ac7540cc346e6518f
s18774 <s18774@pjwstk.edu.pl>
2023-12-11 18:22:25 +0100

    fixed repos

--- src/main/java/pl/wroblewski/helpdeskapp/repositories/UserApplicationRepository.java
+++ src/main/java/pl/wroblewski/helpdeskapp/repositories/UserApplicationRepository.java
@@ -2,7 +2,8 @@ package pl.wroblewski.helpdeskapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import pl.wroblewski.helpdeskapp.models.UserApplication;
+import pl.wroblewski.helpdeskapp.models.UserApplicationId;
 
-public interface UserApplicationRepository extends CrudRepository<UserApplication, Integer> {
+public interface UserApplicationRepository extends CrudRepository<UserApplication, UserApplicationId> {
     //wyszukujemy wnioski konkretnego użytkownika
 }
--- src/main/java/pl/wroblewski/helpdeskapp/repositories/UserTicketRepository.java
+++ src/main/java/pl/wroblewski/helpdeskapp/repositories/UserTicketRepository.java
@@ -2,8 +2,9 @@ package pl.wroblewski.helpdeskapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import pl.wroblewski.helpdeskapp.models.UserTicket;
+import pl.wroblewski.helpdeskapp.models.UserTicketId;
 
-public interface UserTicketRepository extends CrudRepository<UserTicket, Integer> {
+public interface UserTicketRepository extends CrudRepository<UserTicket, UserTicketId> {
     //wyszukiwanie zgłoszeń dla konkretnego użytkownika
 
 }
